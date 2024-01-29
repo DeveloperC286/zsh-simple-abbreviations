a5b39e9350bc6055cf1520bdb10cde3606bdcdc4
DeveloperC <DeveloperC@protonmail.com>
2024-01-29 22:51:17 +0000

    test: adding end to end tests

---
+++ end-to-end-tests/setting-abbreviation.py
@@ -0,0 +1,31 @@
+import pexpect
+import os
+
+
+full_path = os.path.realpath(__file__)
+test_directory = os.path.dirname(full_path)
+
+zsh = pexpect.spawnu('/usr/bin/env zsh --no-rcs',
+                     env=os.environ | {'PROMPT': '>'})
+
+# Ready to take a command.
+zsh.expect('>')
+# Source the plugin and add an abbreviation.
+zsh.sendline(
+    f"source \"{test_directory}/../zsh-simple-abbreviations.zsh\" && zsh-simple-abbreviations --set H 'hello'")
+
+# Ready to take a command.
+zsh.expect('>')
+before = zsh.after
+# Use the abbreviation.
+zsh.sendline("echo H ")
+
+# Ready to take a command.
+zsh.expect('>')
+output = (before + zsh.before)
+
+# Assert the abbreviation was expanded to 'hello'.
+assert "hello" in output
+
+# Done with test close Zsh.
+zsh.close()
