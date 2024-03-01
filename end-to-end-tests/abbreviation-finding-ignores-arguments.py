01fb7b955b0c31f525a4f059c1c4f71c24bd2acf
DeveloperC <DeveloperC@protonmail.com>
2024-03-01 16:37:14 +0000

    fix: not expanding on arguments or environment variables
    The commit[1] removed '-' and '_' from the regex of finding a potential
    abbreviation to expand. So when presented with '-C' it took 'C' and tried to
    find an abbreviation and expand that, which is not the desired behaviour as it
    is part of a CLI argument.
    
    Closes https://github.com/DeveloperC286/zsh-simple-abbreviations/issues/37
    
    * [1] https://github.com/DeveloperC286/zsh-simple-abbreviations/commit/46674b98e27976fb0afaeb51a9e5bb51e1de720d

---
+++ end-to-end-tests/abbreviation-finding-ignores-arguments.py
@@ -0,0 +1,32 @@
+import pexpect
+import os
+
+
+full_path = os.path.realpath(__file__)
+test_directory = os.path.dirname(full_path)
+
+zsh = pexpect.spawnu('/usr/bin/env zsh --no-rcs',
+                     env=os.environ | {'PROMPT': '>'})
+
+# Ready to take a command.
+zsh.expect('>')
+# Source the plugin and add an abbreviation.
+zsh.sendline(
+    f"source \"{test_directory}/../zsh-simple-abbreviations.zsh\" && zsh-simple-abbreviations --set C 'clear'")
+
+# Ready to take a command.
+zsh.expect('>')
+before = zsh.after
+# Use the abbreviation.
+zsh.sendline("echo 'rg -C 123'")
+
+# Ready to take a command.
+zsh.expect('>')
+output = (before + zsh.before)
+
+# Assert the abbreviation was expanded to 'hello'.
+print(output)
+assert "rg -clear 123" not in output
+
+# Done with test close Zsh.
+zsh.close()
